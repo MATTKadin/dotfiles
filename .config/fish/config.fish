b811ec6f033c8468193347fdba531ff1e1ec6928
MATTKadin <matm64@proton.me>
2025-11-05 20:07:00 +0100

    Update config.fish
     from main MATTK-PC

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,108 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
-#starship
-starship init fish | source
+## Set values
+## Run fastfetch as welcome message
+function fish_greeting
+    fastfetch
+end
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+# Format man pages
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
 
-# aliases
+# Append common directories for executable files to $PATH
+fish_add_path ~/.local/bin ~/.cargo/bin ~/Applications/depot_tools
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Copy DIR1 DIR2
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+        set from (echo $argv[1] | trim-right /)
+        set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+## Useful aliases
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+
+# Common use
+alias grubup="sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
 alias snano='sudo nano'
-alias cp='cp -i'
-#alias rm='trash -v'
+alias cp='pycp'
+alias rm='trash -v'
 alias mkdir='mkdir -p'
 alias cls='clear'
 alias ..='cd ..'
-alias rmd='/bin/rm --recursive --force --verbose'
-alias ls='eza -al --color=always --group-directories-first --icons'
-alias lsa='ls -la --color=always'
-
-#Bindings
-    #bind Ctrl+fsudo pacman '-Syu'='sudo pacman -Syu'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
 
 #Bindings
     #bind Ctrl+f to insert 'cdi'
@@ -38,4 +118,14 @@ alias lsa='ls -la --color=always'
 export EDITOR=nano
 export VISUAL=nano
 
+#activate starship and zoxide
+starship init fish | source
 zoxide init --cmd cd fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/mattk/.lmstudio/bin
+# End of LM Studio CLI section
+
+
+# opencode
+fish_add_path /home/mattk/.opencode/bin
